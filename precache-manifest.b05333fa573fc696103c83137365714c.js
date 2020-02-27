b33cb32a8f504d2113ed73c4bcc933ed6a0be83c
elanh <elanh@stanford.edu>
2020-02-27 15:19:32 -0800

    Updates

---
+++ precache-manifest.b05333fa573fc696103c83137365714c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a996d2e297162381b9529059036e9c7",
+    "url": "/cs210/ABC-Virtual-Anchor/index.html"
+  },
+  {
+    "revision": "54cf31ce6d8becd84383",
+    "url": "/cs210/ABC-Virtual-Anchor/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8f45991789fa58b4cc8d",
+    "url": "/cs210/ABC-Virtual-Anchor/static/js/2.31c9ac99.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/cs210/ABC-Virtual-Anchor/static/js/2.31c9ac99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54cf31ce6d8becd84383",
+    "url": "/cs210/ABC-Virtual-Anchor/static/js/main.61770b65.chunk.js"
+  },
+  {
+    "revision": "ccb48fffd8fd932ed2f5",
+    "url": "/cs210/ABC-Virtual-Anchor/static/js/runtime-main.26d99dc4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cs210/ABC-Virtual-Anchor/static/media/logo.5d5d9eef.svg"
+  }
+]);
